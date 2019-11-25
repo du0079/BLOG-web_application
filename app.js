87692222f225a325778661c1969c26a59e5849a0
Deepesh upadhayay <41473554+du0079@users.noreply.github.com>
2019-11-26 02:51:26 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-var bodyparser =require("body-parser");
+var bodyparser =require("body-parser"),
+    express    = require("express"),
+    mongoose   = require("mongoose"),
+    app= express();
+
+// App congiguration==========================================================================================
+app.set("view engine","ejs");
+app.use(express.static("public"));
+mongoose.connect('mongodb://localhost/Blog_app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+ 
+//Database configuration================================================================================================
+var BlogSchema = new mongoose.Schema({
+           title : String,
+           image: String,
+           body: String,
+           Created : {type: Date, default: Date.now }
+});
+
+//Database Model==========================================================================================================
+var Blog = mongoose.model("Blog",BlogSchema);
+
+// Blog.create({
+// 	title: "Blockchain",
+// 	image:"https://images.unsplash.com/photo-1561451213-d5c9f0951fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80",
+// 	body: " A blockchain, originally block chain, is a growing list of records, called blocks, that are linked using cryptography. Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data. By design, a blockchain is resistant to modification of the data"
+// });
+
+
+
+
+
+
+app.listen(3000,function(){
+ console.log("Tested OK");
+});
